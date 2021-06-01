bb85f8874d444826e45d89d28d1fd59dd39cb154
blackadress <blackadress.01@gmail.com>
2021-06-01 13:13:08 -0500

    trabajo handlers added

--- handlers/trabajo.go
+++ handlers/trabajo.go
@@ -1,18 +1,143 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/blackadress/vaula/models"
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4"
+)
 
 func (a *App) getTrabajoByIdHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("GET %s code: %d ERROR: %s -- strconv", r.RequestURI,
+			http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusBadRequest, "ID de trabajo invalido")
+		return
+	}
+
+	trabajo := models.Trabajo{ID: id}
+	err = trabajo.GetTrabajo(a.DB)
+	if err != nil {
+		switch err {
+		case pgx.ErrNoRows:
+			log.Printf("GET %s code: %d ERROR: %s -- no rows", r.RequestURI,
+				http.StatusNotFound, err.Error())
+			respondWithError(w, http.StatusNotFound, "Trabajo no encontrado")
+		default:
+			log.Printf("GET %s code: %d ERROR: %s -- wtf just happened? default switch",
+				r.RequestURI, http.StatusInternalServerError, err.Error())
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	log.Printf("GET %s code: %d", r.RequestURI, http.StatusOK)
+	respondWithJSON(w, http.StatusOK, trabajo)
+	return
 }
 
 func (a *App) getTrabajosHandler(w http.ResponseWriter, r *http.Request) {
+	trabajos, err := models.GetTrabajos(a.DB)
+	if err != nil {
+		log.Printf("GET %s code: %d ERROR: %s -- models.GetTrabajos", r.RequestURI,
+			http.StatusInternalServerError, err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Printf("GET %s code: %d", r.RequestURI, http.StatusOK)
+	respondWithJSON(w, http.StatusOK, trabajos)
+	return
+
 }
 
 func (a *App) createTrabajoHandler(w http.ResponseWriter, r *http.Request) {
+	var trabajo models.Trabajo
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&trabajo)
+	if err != nil {
+		log.Printf("POST %s code: %d ERROR: %s -- decoder", r.RequestURI,
+			http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusBadRequest, "Invalid payload")
+		return
+	}
+	defer r.Body.Close()
+
+	// hay la request debe especificamente settear el valor de trabajo.Activo,
+	// debido a que por defecto se inicializa en 'false'
+	err = trabajo.CreateTrabajo(a.DB)
+	if err != nil {
+		log.Printf("POST %s code: %d ERROR: %s -- trabajo.createTrabajo", r.RequestURI,
+			http.StatusInternalServerError, err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Printf("POST %s code: %d", r.RequestURI, http.StatusCreated)
+	respondWithJSON(w, http.StatusCreated, trabajo)
+	return
 }
 
 func (a *App) updateTrabajoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("PUT %s code: %d ERROR: %s -- strconv", r.RequestURI,
+			http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusBadRequest, "ID de Trabajo invalido")
+		return
+	}
+
+	var trabajo models.Trabajo
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&trabajo)
+	if err != nil {
+		log.Printf("PUT %s code: %d ERROR: %s -- decoder", r.RequestURI,
+			http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusBadRequest, "Invalid payload")
+		return
+	}
+	defer r.Body.Close()
+
+	trabajo.ID = id
+	err = trabajo.UpdateTrabajo(a.DB)
+	if err != nil {
+		log.Printf("PUT %s code: %d ERROR: %s trabajo.UpdateTrabajo", r.RequestURI,
+			http.StatusInternalServerError, err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Printf("PUT %s code: %d", r.RequestURI, http.StatusOK)
+	respondWithJSON(w, http.StatusOK, trabajo)
+	return
 }
 
 func (a *App) deleteTrabajoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("DELETE %s code: %d ERROR: %s -- strconv", r.RequestURI,
+			http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusBadRequest, "ID de trabajo invalido")
+		return
+	}
+
+	trabajo := models.Trabajo{ID: id}
+	if err := trabajo.DeleteTrabajo(a.DB); err != nil {
+		log.Printf("DELETE %s code: %d ERROR: %s -- trabajo.DeleteTrabajo", r.RequestURI,
+			http.StatusInternalServerError, err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Printf("DELETE %s code: %d", r.RequestURI, http.StatusOK)
+	respondWithJSON(w, http.StatusOK, map[string]int{"exito": 1, "id": trabajo.ID})
+	return
 }
