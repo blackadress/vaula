c3ef6db080cf0a6169ba4214885243e7387ee76e
blackadress <blackadress.01@gmail.com>
2021-06-16 17:24:40 -0500

    testing a token invalido

--- backend/handlers/usuario_test.go
+++ backend/handlers/usuario_test.go
@@ -32,6 +32,18 @@ func TestEmptyUsuarioTable(t *testing.T) {
 	}
 }
 
+func TestUnauthorizedToken(t *testing.T) {
+	utils.ClearTableUsuario(a.DB)
+	ensureAuthorizedUserExists()
+
+	token_str := "Bearer token_invalido"
+	req, _ := http.NewRequest("GET", "/users", nil)
+	req.Header.Set("Authorization", token_str)
+	response := executeRequest(req, a)
+
+	checkResponseCode(t, http.StatusUnauthorized, response.Code)
+}
+
 func TestGetNonExistentUsuario(t *testing.T) {
 	utils.ClearTableUsuario(a.DB)
 	ensureAuthorizedUserExists()
@@ -93,148 +105,96 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
-// func TestGetUser(t *testing.T) {
-// 	clearTableUsuario()
-// 	addUsers(1)
-// 	ensureAuthorizedUserExists()
+func TestGetUser(t *testing.T) {
+	utils.ClearTableUsuario(a.DB)
+	utils.AddUsers(1, a.DB)
+	ensureAuthorizedUserExists()
 
-// 	token := getTestJWT()
-// 	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
+	token := getTestJWT()
+	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
 
-// 	req, _ := http.NewRequest("GET", "/users/1", nil)
-// 	req.Header.Set("Authorization", token_str)
-// 	response := executeRequest(req, a)
+	req, _ := http.NewRequest("GET", "/users/1", nil)
+	req.Header.Set("Authorization", token_str)
+	response := executeRequest(req, a)
 
-// 	checkResponseCode(t, http.StatusOK, response.Code)
-// }
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
 
-// func TestUpdateUser(t *testing.T) {
-// 	clearTableUsuario()
-// 	addUsers(1)
-// 	ensureAuthorizedUserExists()
+func TestUpdateUser(t *testing.T) {
+	utils.ClearTableUsuario(a.DB)
+	utils.AddUsers(1, a.DB)
+	ensureAuthorizedUserExists()
 
-// 	token := getTestJWT()
-// 	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
+	token := getTestJWT()
+	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
 
-// 	req, _ := http.NewRequest("GET", "/users/1", nil)
-// 	req.Header.Set("Authorization", token_str)
-// 	response := executeRequest(req, a)
-// 	var originalUser map[string]interface{}
-// 	json.Unmarshal(response.Body.Bytes(), &originalUser)
+	req, _ := http.NewRequest("GET", "/users/1", nil)
+	req.Header.Set("Authorization", token_str)
+	response := executeRequest(req, a)
+	var originalUser map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalUser)
 
-// 	var jsonStr = []byte(`{
-// 		"username": "user_test_updated",
-// 		"password": "1234_updated",
-// 		"email": "user_test_updated@test.ts"}`)
+	var jsonStr = []byte(`{
+		"username": "user_test_updated",
+		"password": "1234_updated",
+		"email": "user_test_updated@test.ts"}`)
 
-// 	req, _ = http.NewRequest("PUT", "/users/1", bytes.NewBuffer(jsonStr))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Authorization", token_str)
-// 	response = executeRequest(req, a)
+	req, _ = http.NewRequest("PUT", "/users/1", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", token_str)
+	response = executeRequest(req, a)
 
-// 	checkResponseCode(t, http.StatusOK, response.Code)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-// 	var m map[string]interface{}
-// 	json.Unmarshal(response.Body.Bytes(), &m)
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
 
-// 	if m["id"] != originalUser["id"] {
-// 		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
-// 	}
+	if m["id"] != originalUser["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
+	}
 
-// 	if m["username"] == originalUser["username"] {
-// 		t.Errorf(
-// 			"Expected the username to change from '%s' to '%s'. Got '%v'",
-// 			originalUser["username"],
-// 			m["username"],
-// 			originalUser["username"],
-// 		)
-// 	}
+	if m["username"] == originalUser["username"] {
+		t.Errorf(
+			"Expected the username to change from '%s' to '%s'. Got '%v'",
+			originalUser["username"],
+			m["username"],
+			originalUser["username"],
+		)
+	}
 
-// 	if m["password"] == originalUser["password"] {
-// 		t.Errorf(
-// 			"Expected the password to change from '%s' to '%s'. Got '%v'",
-// 			originalUser["password"],
-// 			m["password"],
-// 			originalUser["password"],
-// 		)
-// 	}
+	if m["password"] == originalUser["password"] {
+		t.Errorf(
+			"Expected the password to change from '%s' to '%s'. Got '%v'",
+			originalUser["password"],
+			m["password"],
+			originalUser["password"],
+		)
+	}
 
-// 	if m["email"] == originalUser["email"] {
-// 		t.Errorf(
-// 			"Expected the email to change from '%s', to '%s'. Got '%v'",
-// 			originalUser["email"],
-// 			m["email"],
-// 			originalUser["email"],
-// 		)
-// 	}
-// }
+	if m["email"] == originalUser["email"] {
+		t.Errorf(
+			"Expected the email to change from '%s', to '%s'. Got '%v'",
+			originalUser["email"],
+			m["email"],
+			originalUser["email"],
+		)
+	}
+}
 
-// func TestDeleteUser(t *testing.T) {
-// 	clearTableUsuario()
-// 	addUsers(1)
-// 	ensureAuthorizedUserExists()
-// 	token := getTestJWT()
-// 	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
+func TestDeleteUser(t *testing.T) {
+	utils.ClearTableUsuario(a.DB)
+	utils.AddUsers(1, a.DB)
+	ensureAuthorizedUserExists()
+	token := getTestJWT()
+	token_str := fmt.Sprintf("Bearer %s", token.AccessToken)
 
-// 	req, _ := http.NewRequest("GET", "/users/1", nil)
-// 	req.Header.Set("Authorization", token_str)
-// 	response := executeRequest(req, a)
-// 	checkResponseCode(t, http.StatusOK, response.Code)
+	req, _ := http.NewRequest("GET", "/users/1", nil)
+	req.Header.Set("Authorization", token_str)
+	response := executeRequest(req, a)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-// 	req, _ = http.NewRequest("DELETE", "/users/1", nil)
-// 	req.Header.Set("Authorization", token_str)
-// 	response = executeRequest(req, a)
-// 	checkResponseCode(t, http.StatusOK, response.Code)
-// }
-
-// func TestRefreshToken(t *testing.T) {
-// 	clearTableUsuario()
-// 	addUsers(1)
-// 	ensureAuthorizedUserExists()
-// 	//token := getTestJWT()
-
-// }
-
-// const tableCreationQuery = `
-// CREATE TABLE IF NOT EXISTS usuarios
-// 	(
-// 		id INT PRIMARY KEY NOT NULL,
-// 		username TEXT NOT NULL,
-// 		password TEXT NOT NULL,
-// 		email TEXT NOT NULL,
-
-// 		activo BOOLEAN,
-// 		createdAt TIMESTAMPTZ,
-// 		updatedAt TIMESTAMPTZ
-// 	)
-// `
-
-// func ensureTableUsuarioExists() {
-// 	if _, err := a.DB.Exec(context.Background(), tableCreationQuery); err != nil {
-// 		log.Printf("TEST: error creando tabla de usuarios: %s", err)
-// 	}
-// }
-
-// func clearTableUsuario() {
-// 	a.DB.Exec(context.Background(), "DELETE FROM usuarios")
-// 	a.DB.Exec(context.Background(), "ALTER SEQUENCE usuarios_id_seq RESTART WITH 1")
-// }
-
-// func addUsers(count int) {
-// 	now := time.Now()
-// 	if count < 1 {
-// 		count = 1
-// 	}
-
-// 	for i := 0; i < count; i++ {
-// 		a.DB.Exec(
-// 			context.Background(),
-// 			`INSERT INTO usuarios(username, password, email, activo, createdAt, updatedAt)
-// 			VALUES($1, $2, $3, $4, $5, $6)`,
-// 			"user_"+strconv.Itoa(i),
-// 			"pass"+strconv.Itoa(i),
-// 			"em"+strconv.Itoa(i)+"@test.ts",
-// 			i%2 == 0, now, now,
-// 		)
-// 	}
-// }
+	req, _ = http.NewRequest("DELETE", "/users/1", nil)
+	req.Header.Set("Authorization", token_str)
+	response = executeRequest(req, a)
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
